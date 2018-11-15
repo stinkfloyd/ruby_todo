b52f79eac9dd08a8870d4a3239292236af0e4989
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-11-14 17:08:59 -0700

    "Html to HAML conversion"

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,43 @@
 class TasksController < ApplicationController
   def new
     @task = Task.new
+    render :show_form
   end
+
   def create
-    @task = Task.create(task_params)
+    @task = Task.new(task_params)
+    save_task
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.assign_attributes(task_params)
+    save_task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
     @tasks = Task.all
   end
+
+  def edit
+    @task = Task.find(params[:id])
+    render :show_form
+  end
+
   private
+  def save_task
+    if @task.save
+      @tasks = Task.all
+      render :hide_form
+    else
+      render :show_form
+    end
+  end
+
   def task_params
     params.require(:task).permit(:title, :note, :completed)
   end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
-class Task < ApplicationRecord
+class Task < ActiveRecord::Base
+  validates_presence_of :title
+  validate :future_completed_date
+
+  private
+
+  def future_completed_date
+    if !completed.blank? && completed > Date.today
+      self.errors.add(:completed, "Cannot pick a future date.")
+    end
+  end
 end
