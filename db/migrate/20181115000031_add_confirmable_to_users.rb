fa71f61a9a17a862639ece4b154d7c56008f7f55
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-11-15 22:48:21 -0700

    "Heroku"

--- db/migrate/20181115000031_add_confirmable_to_users.rb
+++ db/migrate/20181115000031_add_confirmable_to_users.rb
@@ -1,9 +1,9 @@
 class AddConfirmableToUsers < ActiveRecord::Migration[5.2]
       # Note: You can't use change, as User.update_all with fail in the down migration
       def self.up
-        add_column :users, :confirmation_token, :string
-        add_column :users, :confirmed_at, :datetime
-        add_column :users, :confirmation_sent_at, :datetime
+        #add_column :users, :confirmation_token, :string
+        #add_column :users, :confirmed_at, :datetime
+        #add_column :users, :confirmation_sent_at, :datetime
         # add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
         add_index :users, :confirmation_token, :unique => true
         # User.reset_column_information # Need for some types of updates, but not for update_all.
