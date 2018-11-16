a00110cfe8f2d93034a58e74e323ca9b393ed763
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-11-15 19:53:55 -0700

    "About to test email from production"

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 gem  'bootstrap-datepicker-rails'
 gem 'devise'
 gem 'cancan'
+gem 'dotenv-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'http://todo-yourdomainhere.rhcloud.com' }
+  config.action_mailer.asset_host = 'http://todo-yourdomainhere.rhcloud.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mail.yahoo.com',
+    port: 465,
+    user_name: ENV["YAHOO_USERNAME"]+'@yahoo.com',
+    password: ENV["YAHOO_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
